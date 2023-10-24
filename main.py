0d809ef15ecd10ceeb00918b7383d14da6357be6
ArtemSermyagin <artem.sermyagin@yahoo.com>
2023-10-24 19:27:18 +0400

    7000 79** **** 6361 # function output

---
+++ main.py
@@ -0,0 +1,11 @@
+from src.widget import print_card_check, print_number
+
+
+def main():
+    print(print_card_check(type_data), print_number(number, type_data))
+
+
+type_data = "Visa"
+number = "1596837868705199"
+
+main()
