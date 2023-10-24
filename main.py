0bec289243d90879678e71862587e586123241d9
ArtemSermyagin <artem.sermyagin@yahoo.com>
2023-10-24 19:44:55 +0400

    Указал docstring

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from src.widget import print_card_check, print_number
 
 
 def main():
+    """
+    Выводит на печать вид и номер
+    """
     print(print_card_check(type_data), print_number(number, type_data))
 
 
