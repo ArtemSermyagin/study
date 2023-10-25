4ecc356bb4a425fb73b9cc8e9e4a9d395a47760e
ArtemSermyagin <artem.sermyagin@yahoo.com>
2023-10-25 22:36:58 +0400

    Удалил все данные из main

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-from src.widget import print_card_check, print_number
 
-
-def main():
-    """
-    Выводит на печать вид и номер
-    """
-    print(print_card_check(type_data), print_number(number, type_data))
-
-
-type_data = "Visa"
-number = "1596837868705199"
-
-main()
